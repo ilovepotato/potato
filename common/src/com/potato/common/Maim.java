1ba9c618962016a69f695a6ee382ca273e3b56f5
wangshiyu <297376161@qq.com>
2015-08-13 00:23:14 +0800

    Initial commit

---
+++ common/src/com/potato/common/Maim.java
@@ -0,0 +1,11 @@
+package com.potato.common;
+
+/**
+ * Created by Administrator on 2015/7/30.
+ */
+public class Maim {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
