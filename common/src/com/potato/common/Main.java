c71af29629c2a34d7bd2243172a830cc9d47f623
wangshiyu <297376161@qq.com>
2015-08-13 01:30:46 +0800

    test

--- common/src/com/potato/common/Main.java
+++ common/src/com/potato/common/Main.java
@@ -3,9 +3,10 @@ package com.potato.common;
 /**
  * Created by Administrator on 2015/7/30.
  */
-public class Maim {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, world");
+
     }
 }
